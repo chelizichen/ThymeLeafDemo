feae2ca1cb2941b3d2bc58f9996ea33dc641efe0
chelizichen <1347290221@qq.com>
2023-05-29 01:04:17 +0800

    update:不同数据结构的对比

--- src/main/java/com/example/demo/controller/URLController.java
+++ src/main/java/com/example/demo/controller/URLController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class URLController {
@@ -11,5 +17,24 @@ public class URLController {
     {
         model.addAttribute("name","bigsai");
         return "index";//与templates中index.html对应
+    }
+
+    @GetMapping("test")
+    public String getTest(Model model)//对应函数
+    {
+        User user1=new User("bigsai",22,"一个幽默且热爱java的社会青年");
+        List<String> userList=new ArrayList<>();
+        userList.add("zhang san 66");
+        userList.add("li si 66");
+        userList.add("wang wu 66");
+        Map<String ,String> map=new HashMap<>();
+        map.put("place","博学谷");
+        map.put("feeling","very well");
+        //数据添加到model中
+        model.addAttribute("name","bigsai");//普通字符串
+        model.addAttribute("user",user1);//储存javabean
+        model.addAttribute("userlist",userList);//储存List
+        model.addAttribute("map",map);//储存Map
+        return "test";//与templates中index.html对应
     }
 }
